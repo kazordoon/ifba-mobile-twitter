88fecc9b015e66707a98cf751cbef3c4c1d282d3
Felipe Barros <felipetech@tuta.io>
2024-08-18 19:43:48 -0300

    feat: Add account registration screen

--- screens/RegistrationScreen.js
+++ screens/RegistrationScreen.js
@@ -1,12 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, Button } from 'react-native';
-import styles from '../styles';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import React, { useState } from 'react';
+import { colors } from '../styles';
 
-export default function RegistrationScreen() {
+export default function RegistrationScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+
   return (
-    <View style={ styles.container }>
-      <Text style={styles.title}>Registro</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}>
+      <Text style={styles.label}>Registre sua conta</Text>
+      <TextInput
+        placeholder="E-mail"
+        placeholderTextColor="rgba(52, 52, 52, 0.8)"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+      />
+      <TextInput
+      secureTextEntry={true}
+        placeholder="Senha"
+        placeholderTextColor="rgba(52, 52, 52, 0.8)"
+        value={password}
+        onChangeText={setPassword}
+        style={styles.input}
+      />
+      <TextInput
+      secureTextEntry={true}
+        placeholder="Senha"
+        placeholderTextColor="rgba(52, 52, 52, 0.8)"
+        value={passwordConfirmation}
+        onChangeText={setPasswordConfirmation}
+        style={styles.input}
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Feed')}
+      >
+        <Text
+          style={styles.buttonText}
+        >
+          Cadastrar
+        </Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.primaryBg,
+    flex: 1,
+    justifyContent: 'center',
+    padding: 24,
+  },
+  label: {
+    color: '#fff',
+    fontSize: 24,
+    marginVertical: 5,
+  },
+  input: {
+    color: '#000',
+    borderColor: '#000',
+    borderWidth: StyleSheet.hairlineWidth,
+    padding: 10,
+    fontSize: 20,
+    marginVertical: 5,
+    borderRadius: 10,
+    backgroundColor: '#fff'
+  },
+  button: {
+    backgroundColor: '#050A12',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    marginVertical: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
