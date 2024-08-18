a49213b859c8723f2ec32e80c3bf2617d88de2e5
Felipe Barros <felipetech@tuta.io>
2024-08-18 16:58:31 -0300

    feat: Add Post component

---
+++ components/IconButton.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import { EvilIcons } from '@expo/vector-icons';
+
+const IconButton = ({ icon, text }) => {
+  return (
+    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <EvilIcons name={icon} size={22} color="gray" />
+      <Text style={{ fontSize: 12, color: 'gray' }}>{text}</Text>
+    </View>
+  );
+};
+
+export default IconButton;
---
+++ components/Post.js
@@ -0,0 +1,70 @@
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
+import IconButton from './IconButton';
+
+export default function Post({ post }) {
+  return (
+    <View>
+      <Pressable style={styles.container}>
+        <Image source={post.user.image} style={styles.userImage} />
+
+        <View style={styles.mainContainer}>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={styles.name}>{post.user.name}</Text>
+            <Text style={styles.username}>{post.user.username} · 1h</Text>
+          </View>
+
+          <Text style={styles.content}>{post.content}</Text>
+
+          {post.image && <Image source={post.image} style={styles.image} />}
+
+          <View style={styles.footer}>
+            <IconButton icon="comment" text={post.numberOfComments} />
+            <IconButton icon="retweet" text={post.numberOfRetweets} />
+            <IconButton icon="heart" text={post.numberOfLikes} />
+          </View>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    padding: 10,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderColor: 'lightgrey',
+    backgroundColor: 'white',
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+  },
+  mainContainer: {
+    marginLeft: 10,
+    flex: 1,
+  },
+  name: {
+    fontWeight: '600',
+  },
+  username: {
+    color: 'gray',
+    marginLeft: 5,
+  },
+  content: {
+    lineHeight: 20,
+    marginTop: 5,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 16 / 9,
+    marginVertical: 10,
+    borderRadius: 15,
+  },
+  footer: {
+    flexDirection: 'row',
+    marginVertical: 5,
+    justifyContent: 'space-between',
+  },
+});
